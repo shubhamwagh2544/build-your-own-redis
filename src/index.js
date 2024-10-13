97ebdf839a823aae61e13ea12e19c85311a5a8ab
thehead <shubhamwagh2544@gmail.com>
2024-10-13 12:25:15 +0530

    lpush rpush lpop rpop llen: redis

--- src/index.js
+++ src/index.js
@@ -1,21 +1,13 @@
-const net = require('net');
-const {
+let {
     deserializer,
     serializer,
     redisRegexPattern,
     checkKeysExists,
-    processBulkString, deleteMultipleKeys
+    processBulkString, deleteMultipleKeys, redisInfo, serverInfo, clientsInfo, memoryInfo, statsInfo, replicationInfo,
+    cpuInfo, keyspaceInfo, port, hostname, redisUrl, totalCommandsProcessed, store, server, checkIfKeyIsList
 } = require("./util");
-const {version} = require('../package.json');
+const {isNil} = require("lodash");
 
-const server = net.createServer();
-const port = 8000;
-const hostname = '127.0.0.1';
-const redisUrl = `https://redis.io/learn/howtos/quick-start/cheat-sheet`;
-let totalCommandsProcessed = 0;
-
-const store = {};
-console.log(version);
 
 server.on('connection', (socket) => {
     console.log('client connected: ', socket.address());
@@ -74,6 +66,12 @@ server.on('connection', (socket) => {
                             socket.write(`$-1\r\n`);
                             return;
                         }
+
+                        if (checkIfKeyIsList(key, store)) {
+                            message = 'WRONGTYPE Operation against a key holding the wrong kind of value';
+                            socket.write(`-${message}\r\n`);
+                        }
+
                         totalCommandsProcessed += 1;
 
                         value = value.toString();
@@ -126,6 +124,11 @@ server.on('connection', (socket) => {
                             return;
                         }
 
+                        if (checkIfKeyIsList(key, store)) {
+                            message = 'WRONGTYPE Operation against a key holding the wrong kind of value';
+                            socket.write(`-${message}\r\n`);
+                        }
+
                         let value = store[key];
                         value = parseInt(value, 10);
                         if (isNaN(value)) {
@@ -141,6 +144,158 @@ server.on('connection', (socket) => {
                         socket.write(`:${value}\r\n`)
                     } else {
                         message = `ERR syntax error`
+                        socket.write(`-${message}\r\n`);
+                    }
+                }
+                    break;
+
+                // LISTS
+                case 'lpush': {
+                    if (reply.length >= 3) {
+                        const key = reply[1];
+
+                        // check if key exists
+                        if (store[key]) {
+                            if (!checkIfKeyIsList(key, store)) {
+                                message = 'WRONGTYPE Operation against a key holding the wrong kind of value';
+                                socket.write(`-${message}\r\n`);
+                                return;
+                            }
+                        } else {
+                            store[key] = [];
+                        }
+
+                        totalCommandsProcessed += 1;
+
+                        const values = reply.slice(2);
+                        store[key].unshift(...values);
+
+                        socket.write(`:${store[key].length}\r\n`);
+                    } else {
+                        message = `ERR wrong number of arguments for ${command} command`
+                        socket.write(`-${message}\r\n`);
+                    }
+                }
+                    break;
+
+                case 'rpush': {
+                    if (reply.length >= 3) {
+                        const key = reply[1];
+
+                        // check if key exists
+                        if (store[key]) {
+                            if (!checkIfKeyIsList(key, store)) {
+                                message = 'WRONGTYPE Operation against a key holding the wrong kind of value';
+                                socket.write(`-${message}\r\n`);
+                                return;
+                            }
+                        } else {
+                            store[key] = [];
+                        }
+
+                        totalCommandsProcessed += 1;
+
+                        const values = reply.slice(2);
+                        store[key].push(...values);
+
+                        socket.write(`:${store[key].length}\r\n`);
+                    } else {
+                        message = `ERR wrong number of arguments for ${command} command`
+                        socket.write(`-${message}\r\n`);
+                    }
+                }
+                    break;
+
+                case 'lrange': {
+
+                }
+                    break;
+
+                case 'llen': {
+                    if (reply.length === 2) {
+                        const key = reply[1];
+
+                        if (!store[key]) {
+                            socket.write(`:0\r\n`);
+                            return;
+                        } else {
+                            if (!checkIfKeyIsList(key, store)) {
+                                message = 'WRONGTYPE Operation against a key holding the wrong kind of value';
+                                socket.write(`-${message}\r\n`);
+                                return;
+                            }
+
+                            totalCommandsProcessed += 1;
+
+                            const length = store[key].length;
+                            socket.write(`:${length}\r\n`);
+                        }
+                    } else {
+                        message = `ERR wrong number of arguments for ${command} command`
+                        socket.write(`-${message}\r\n`);
+                    }
+                }
+                    break;
+
+                case 'lpop': {
+                    if (reply.length === 2 || reply.length === 3) {
+                        const key = reply[1];
+                        let count = isNil(reply[2]) ? 1 : reply[2];
+
+                        if (!store[key]) {
+                            socket.write(`$-1\r\n`);
+                            return;
+                        } else {
+                            if (!checkIfKeyIsList(key, store)) {
+                                message = 'WRONGTYPE Operation against a key holding the wrong kind of value';
+                                socket.write(`-${message}\r\n`);
+                                return;
+                            }
+
+                            totalCommandsProcessed += 1;
+
+                            // delete multiple elements
+                            if (count > store[key].length) {
+                                count = store[key].length;
+                            }
+                            let values = store[key].splice(0, count);
+                            values = deserializer(values);
+                            socket.write(values);
+                        }
+                    } else {
+                        message = `ERR wrong number of arguments for ${command} command`
+                        socket.write(`-${message}\r\n`);
+                    }
+                }
+                    break;
+
+                case 'rpop': {
+                    if (reply.length === 2 || reply.length === 3) {
+                        const key = reply[1];
+                        let count = isNil(reply[2]) ? 1 : reply[2];
+
+                        if (!store[key]) {
+                            socket.write(`$-1\r\n`);
+                            return;
+                        } else {
+                            if (!checkIfKeyIsList(key, store)) {
+                                message = 'WRONGTYPE Operation against a key holding the wrong kind of value';
+                                socket.write(`-${message}\r\n`);
+                                return;
+                            }
+
+                            totalCommandsProcessed += 1;
+
+                            // delete multiple elements
+                            if (count > store[key].length) {
+                                count = store[key].length;
+                            }
+                            let values = store[key].splice(-count, count);
+                            values = deserializer(values);
+                            socket.write(values);
+                        }
+                    } else {
+                        message = `ERR wrong number of arguments for ${command} command`
                         socket.write(`-${message}\r\n`);
                     }
                 }
@@ -177,6 +332,21 @@ server.on('connection', (socket) => {
                 }
                     break;
 
+                case 'expire': {
+
+                }
+                    break;
+
+                case 'ttl': {
+
+                }
+                    break;
+
+                case 'persist': {
+
+                }
+                    break;
+
                 // CUSTOM
                 case 'store': {
                     // print all keys and values present in store
@@ -188,48 +358,69 @@ server.on('connection', (socket) => {
                     break;
 
                 // INFO
-                /*case 'info': {
-                    if (reply.length > 1) {
+                case 'info': {
+                    if (reply.length === 1) {
+                        totalCommandsProcessed += 1;
+                        const result = processBulkString(redisInfo);
+                        socket.write(result);
+                    } else if (reply.length === 2) {
+                        const section = reply[1];
+
+                        switch (section) {
+                            case 'server': {
+                                totalCommandsProcessed += 1;
+                                const result = processBulkString(serverInfo);
+                                socket.write(result);
+                            }
+                                break;
+                            case 'clients': {
+                                totalCommandsProcessed += 1;
+                                const result = processBulkString(clientsInfo);
+                                socket.write(result);
+                            }
+                                break;
+                            case 'memory': {
+                                totalCommandsProcessed += 1;
+                                const result = processBulkString(memoryInfo);
+                                socket.write(result);
+                            }
+                                break;
+                            case 'stats': {
+                                totalCommandsProcessed += 1;
+                                const result = processBulkString(statsInfo);
+                                socket.write(result);
+                            }
+                                break;
+                            case 'replication': {
+                                totalCommandsProcessed += 1;
+                                const result = processBulkString(replicationInfo);
+                                socket.write(result);
+                            }
+                                break;
+                            case 'cpu': {
+                                totalCommandsProcessed += 1;
+                                const result = processBulkString(cpuInfo);
+                                socket.write(result);
+                            }
+                                break;
+                            case 'keyspace': {
+                                totalCommandsProcessed += 1;
+                                const result = processBulkString(keyspaceInfo);
+                                socket.write(result);
+                            }
+                                break;
+                            default: {
+                                message = `ERR syntax error`
+                                socket.write(`-${message}\r\n`);
+                            }
+                        }
+
+                    } else {
                         message = `ERR wrong number of arguments for ${command} command`
                         socket.write(`-${message}\r\n`);
                     }
-                    totalCommandsProcessed += 1;
-
-                    let serverInfo =
-                        `
-                        # Server
-                        redis_version: ${version}
-                        os: ${process.platform}-${process.arch}
-                        process_id: ${process.pid}
-                        uptime_in_seconds: ${process.uptime()}
-                        tcp_port: ${port}
-
-                        # Clients
-                        connected_clients: ${server.connections}
-                        blocked_clients: 0
-
-                        # Memory
-                        used_memory: ${process.memoryUsage().heapUsed}
-                        used_memory_human: ${(process.memoryUsage().heapUsed / (1024 * 1024)).toFixed(2)}M
-                        used_memory_peak: ${process.memoryUsage().heapTotal}
-                        used_memory_peak_human: ${(process.memoryUsage().heapTotal / (1024 * 1024)).toFixed(2)}M
-
-                        # Stats
-                        total_connections_received: ${server.connections}
-                        total_commands_processed: ${totalCommandsProcessed}
-
-                        # Replication
-                        role: master
-                        connected_slaves: 0
-
-                        # CPU
-                        used_cpu_sys: ${process.cpuUsage().system}
-                        used_cpu_user: ${process.cpuUsage().user}
-                    `;
-                    //serverInfo = processBulkString(serverInfo);
-                    socket.write(`$${serverInfo.length}\r\n${serverInfo}\r\n`);
                 }
-                    break;*/
+                    break;
 
                 default: {
                     message = `Wrong Command: ${command}. Please visit ${redisUrl}`;
@@ -246,7 +437,6 @@ server.on('connection', (socket) => {
             message = processBulkString(message);
             socket.write(message);
         }
-
     });
 
     server.on('close', () => {
--- src/util.js
+++ src/util.js
@@ -1,3 +1,68 @@
+const net = require('net');
+const {get} = require('lodash');
+const {version} = require('../package.json');
+
+const port = 8000;
+const hostname = '127.0.0.1';
+const redisUrl = `https://redis.io/learn/howtos/quick-start/cheat-sheet`;
+let totalCommandsProcessed = 0;
+
+const server = net.createServer();
+const store = {};
+
+const serverInfo =
+    `
+    # Server
+    redis_version: ${version}
+    os: ${process.platform}-${process.arch}
+    process_id: ${process.pid}
+    uptime_in_seconds: ${process.uptime()}
+    tcp_port: ${port}
+    `;
+
+const clientsInfo =
+    `
+    # Clients
+    connected_clients: ${get(server, 'connections', 0)}
+    blocked_clients: 0
+    `;
+
+const memoryInfo =
+    `
+    # Memory
+    used_memory: ${process.memoryUsage().heapUsed}
+    used_memory_human: ${(process.memoryUsage().heapUsed / (1024 * 1024)).toFixed(2)}M
+    used_memory_peak: ${process.memoryUsage().heapTotal}
+    used_memory_peak_human: ${(process.memoryUsage().heapTotal / (1024 * 1024)).toFixed(2)}M
+    `;
+
+const statsInfo =
+    `
+    # Stats
+    total_connections_received: ${get(server, 'connections', 0)}
+    total_commands_processed: ${totalCommandsProcessed}
+    `;
+
+const replicationInfo =
+    `
+    # Replication
+    role: master
+    connected_slaves: 0
+    `;
+
+const cpuInfo =
+    `
+    # CPU
+    used_cpu_sys: ${process.cpuUsage().system}
+    used_cpu_user: ${process.cpuUsage().user}
+    `;
+
+const keyspaceInfo =
+    `
+    # Keyspace
+    db0:keys=${'0'},expires=0,avg_ttl=0
+    `;
+
 function checkIfKeyExist(key, object) {
     // 1
     // return key in object;
@@ -115,12 +180,32 @@ function deleteMultipleKeys(keys, store) {
     return data;
 }
 
+function checkIfKeyIsList(key, store) {
+    return typeof store[key] === 'object';
+}
+
+
 module.exports = {
-    checkIfKeyExist,
+    port,
+    hostname,
+    redisUrl,
+    totalCommandsProcessed,
+    version,
+    store,
+    server,
+    serverInfo,
+    clientsInfo,
+    memoryInfo,
+    statsInfo,
+    replicationInfo,
+    cpuInfo,
+    keyspaceInfo,
+    redisInfo: serverInfo + clientsInfo + memoryInfo + statsInfo + replicationInfo + cpuInfo + keyspaceInfo,
     deserializer,
     serializer,
     redisRegexPattern,
     checkKeysExists,
     processBulkString,
-    deleteMultipleKeys
+    deleteMultipleKeys,
+    checkIfKeyIsList
 }
